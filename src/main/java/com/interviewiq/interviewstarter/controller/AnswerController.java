05b914ade461d5ecb60f840f0360f15cb7b9d422
waseeyurrahman <wasee@gmail.com>
2026-08-26 23:07:06 +0530

    fix: validate submitted interview answers

--- src/main/java/com/interviewiq/interviewstarter/controller/AnswerController.java
+++ src/main/java/com/interviewiq/interviewstarter/controller/AnswerController.java
@@ -6,6 +6,7 @@ import com.interviewiq.interviewstarter.entity.Answer;
 import com.interviewiq.interviewstarter.exception.ForbiddenException;
 import com.interviewiq.interviewstarter.security.AnswerSecurity;
 import com.interviewiq.interviewstarter.service.AnswerService;
+import jakarta.validation.Valid;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,28 +27,34 @@ public class AnswerController {
     }
 
     @PostMapping
-    public SubmitAnswersResponse submitAnswers(@RequestBody SubmitAnswersRequest request, Authentication authentication){
-        List<Answer> toSave = new ArrayList<>();
-        if (request.getAnswers() != null) {
-            for (SubmitAnswerItem item : request.getAnswers()) {
-                // Verify that the question belongs
-                // to the authenticated user's interview
-                if (!answerSecurity.isQuestionOwner(
-                        item.getQuestionId(),
-                        authentication)) {
+    public SubmitAnswersResponse submitAnswers(
+            @Valid @RequestBody SubmitAnswersRequest request,
+            Authentication authentication) {
 
-                    throw new ForbiddenException(
-                            "You are not allowed to answer this question"
-                    );
-                }
-                Answer a = new Answer();
-                a.setQuestion(answerService.getQuestion(item.getQuestionId()));
-                a.setAnswerText(item.getAnswerText());
-                toSave.add(a);
+        List<Answer> toSave = new ArrayList<>();
+
+        for (SubmitAnswerItem item : request.getAnswers()) {
+
+            if (!answerSecurity.isQuestionOwner(
+                    item.getQuestionId(),
+                    authentication)) {
+
+                throw new ForbiddenException(
+                        "You are not allowed to answer this question"
+                );
             }
+
+            Answer a = new Answer();
+            a.setQuestion(
+                    answerService.getQuestion(item.getQuestionId())
+            );
+            a.setAnswerText(item.getAnswerText());
+
+            toSave.add(a);
         }
+
         List<Answer> saved = answerService.saveAll(toSave);
+
         return new SubmitAnswersResponse(true, saved.size());
     }
-
 }
